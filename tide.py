6762b97ffa868bd36f95e935a35ff565ff4cad0a
Paul Cobbaut <paul@cobbaut.be>
2023-04-25 14:07:16 +0200

    Adding knives and using dict
    Adding rectangles for knives.
    Using dict for each item (instead of several int).

--- tide.py
+++ tide.py
@@ -34,66 +34,45 @@ s_radius = 2  # Side Fillet Radius (somehow cannot be more than 2)
 # forks
 # handle width/length and tines width/length in mm
 # assuming a fork has two parts: a handle and a tines 
-fork14_handw = 15
-fork14_handl = 100
-fork14_tinew = 24
-fork14_tinel = 50
-
-fork15_handw = 15
-fork15_handl = 100
-fork15_tinew = 26
-fork15_tinel = 55
-
-fork18_handw = 18
-fork18_handl = 115
-fork18_tinew = 28
-fork18_tinel = 65
-
-fork20_handw = 20
-fork20_handl = 140
-fork20_tinew = 30
-fork20_tinel = 75
+fork14 = { "handw": 14, "handl": 100, "tinew": 24, "tinel": 50 }
+fork15 = { "handw": 15, "handl": 100, "tinew": 26, "tinel": 55 }
+fork18 = { "handw": 18, "handl": 115, "tinew": 28, "tinel": 65 }
+fork20 = { "handw": 20, "handl": 140, "tinew": 30, "tinel": 75 }
 
 # spoons
 # handle width/length and bowl width/length in mm
 # assuming a spoon has two parts: a handle and a bowl
-spoon13_handw = 14
-spoon13_handl = 90
-spoon13_bowlw = 30
-spoon13_bowll = 46  # bowll odd number == 100% CPU for minutes
+# bowll odd number == 100% CPU for minutes
+spoon13 = { "handw": 14, "handl":  90, "bowlw": 30, "bowll": 46 }
+spoon15 = { "handw": 16, "handl": 100, "bowlw": 35, "bowll": 56 }
+spoon18 = { "handw": 18, "handl": 120, "bowlw": 42, "bowll": 66 }
+spoon21 = { "handw": 20, "handl": 140, "bowlw": 50, "bowll": 76 }
 
-spoon15_handw = 16
-spoon15_handl = 100
-spoon15_bowlw = 35
-spoon15_bowll = 56  # bowll odd number == 100% CPU for minutes
+# knives
+# simple rectangle box for knives
+knive17 = { "width": 30, "length": 170 }
+knive18 = { "width": 32, "length": 180 }
+knive21 = { "width": 34, "length": 215 }
+knive24 = { "width": 36, "length": 245 }
 
-spoon18_handw = 18
-spoon18_handl = 120
-spoon18_bowlw = 42
-spoon18_bowll = 66  # bowll odd number == 100% CPU for minutes
-
-spoon21_handw = 20
-spoon21_handl = 140
-spoon21_bowlw = 50
-spoon21_bowll = 76  # bowll odd number == 100% CPU for minutes
-
+# specials
 
 # creates a sketch for one fork size
-def create_fork_Sketch(label, handw, handl, tinew, tinel):
+def create_fork_Sketch(label, fork):
     BodyLabel   = 'Body_'   + label
     SketchLabel = 'Sketch_' + label
     # create Body and Sketch Object
     Body_obj   = doc.addObject("PartDesign::Body", BodyLabel)
     Sketch_obj = doc.getObject(BodyLabel).newObject("Sketcher::SketchObject", SketchLabel)
     # create points
-    point0 = App.Vector(0,           0,           0)
-    point1 = App.Vector(handw,       0,           0)
-    point2 = App.Vector(handw,       handl,       0)
-    point3 = App.Vector(tinew,       handl,       0)
-    point4 = App.Vector(tinew,       handl+tinel, 0)
-    point5 = App.Vector(handw-tinew, handl+tinel, 0)
-    point6 = App.Vector(handw-tinew, handl,       0)
-    point7 = App.Vector(0,           handl,       0)
+    point0 = App.Vector(0                          , 0                            , 0)
+    point1 = App.Vector(fork["handw"]              , 0                            , 0)
+    point2 = App.Vector(fork["handw"]              , fork["handl"]                , 0)
+    point3 = App.Vector(fork["tinew"]              , fork["handl"]                , 0)
+    point4 = App.Vector(fork["tinew"]              , fork["handl"] + fork["tinel"], 0)
+    point5 = App.Vector(fork["handw"]-fork["tinew"], fork["handl"] + fork["tinel"], 0)
+    point6 = App.Vector(fork["handw"]-fork["tinew"], fork["handl"]                , 0)
+    point7 = App.Vector(0                          , fork["handl"]                , 0)
     # create lines that kinda surround a fork
     Sketch_obj.addGeometry(Part.LineSegment(point0,point1),False)
     Sketch_obj.addGeometry(Part.LineSegment(point1,point2),False)
@@ -106,17 +85,19 @@ def create_fork_Sketch(label, handw, handl, tinew, tinel):
     return Sketch_obj
 
 # create a sketch for one spoon size
-def create_spoon_Sketch(label, handw, handl, bowlw, bowll):
-    BodyLabel = 'Body_' + label
+def create_spoon_Sketch(label, spoon):
+    BodyLabel   = 'Body_'   + label
     SketchLabel = 'Sketch_' + label
+    # added to handle length for overlap with ellipse followed by trim
+    overlap = 40 
     # create Body and Sketch Object
     Body_obj = doc.addObject("PartDesign::Body", BodyLabel)
     Sketch_obj = doc.getObject(BodyLabel).newObject("Sketcher::SketchObject", SketchLabel)
     # create points
-    point0 = App.Vector(0,     0,          0)
-    point1 = App.Vector(handw, 0,          0)
-    point2 = App.Vector(handw, handl + 40, 0)
-    point3 = App.Vector(0,     handl + 40, 0)
+    point0 = App.Vector(0             , 0                       , 0)
+    point1 = App.Vector(spoon["handw"], 0                       , 0)
+    point2 = App.Vector(spoon["handw"], spoon["handl"] + overlap, 0)
+    point3 = App.Vector(0             , spoon["handl"] + overlap, 0)
     # create lines that kinda surround a spoon handle
     Sketch_obj.addGeometry(Part.LineSegment(point0,point1),False)
     Sketch_obj.addGeometry(Part.LineSegment(point1,point2),False)
@@ -124,15 +105,34 @@ def create_spoon_Sketch(label, handw, handl, bowlw, bowll):
     Sketch_obj.addGeometry(Part.LineSegment(point3,point0),False)
     # create an ellipse that kinda surrounds a spoon bowl
     # major axis(bottom point), minor_axis(right point), center
-    e_major  = App.Vector(handw/2,           handl,           0)
-    e_minor  = App.Vector(handw/2 + bowlw/2, handl + bowll/2, 0)
-    e_center = App.Vector(handw/2,           handl + bowll/2, 0)
+    e_major  = App.Vector(spoon["handw"]/2                   , spoon["handl"]                   , 0)
+    e_minor  = App.Vector(spoon["handw"]/2 + spoon["bowlw"]/2, spoon["handl"] + spoon["bowll"]/2, 0)
+    e_center = App.Vector(spoon["handw"]/2                   , spoon["handl"] + spoon["bowll"]/2, 0)
     Sketch_obj.addGeometry(Part.Ellipse(e_major, e_minor, e_center),False)
     # trim the unneeded lines
-    Sketch_obj.trim(2,App.Vector(handw/2, handl+40, 0))
-    Sketch_obj.trim(1,App.Vector(handw,   handl+20, 0))
-    Sketch_obj.trim(2,App.Vector(0,       handl+20, 0))
-    Sketch_obj.trim(3,App.Vector(handw/2, handl,    0))
+    Sketch_obj.trim(2,App.Vector(spoon["handw"]/2, spoon["handl"]+40, 0))
+    Sketch_obj.trim(1,App.Vector(spoon["handw"]  , spoon["handl"]+20, 0))
+    Sketch_obj.trim(2,App.Vector(0               , spoon["handl"]+20, 0))
+    Sketch_obj.trim(3,App.Vector(spoon["handw"]/2, spoon["handl"]   , 0))
+    return Sketch_obj
+
+# create a sketch for one knive size
+def create_knive_Sketch(label, knive):
+    BodyLabel   = 'Body_'   + label
+    SketchLabel = 'Sketch_' + label
+    # create Body and Sketch Object
+    Body_obj   = doc.addObject("PartDesign::Body", BodyLabel)
+    Sketch_obj = doc.getObject(BodyLabel).newObject("Sketcher::SketchObject", SketchLabel)
+    # create points
+    point0 = App.Vector(0             , 0              , 0)
+    point1 = App.Vector(knive["width"], 0              , 0)
+    point2 = App.Vector(knive["width"], knive["length"], 0)
+    point3 = App.Vector(0             , knive["length"], 0)
+    # create lines that form a rectangle
+    Sketch_obj.addGeometry(Part.LineSegment(point0,point1),False)
+    Sketch_obj.addGeometry(Part.LineSegment(point1,point2),False)
+    Sketch_obj.addGeometry(Part.LineSegment(point2,point3),False)
+    Sketch_obj.addGeometry(Part.LineSegment(point3,point0),False)
     return Sketch_obj
 
 # pads a sketch
@@ -186,41 +186,59 @@ def create_Fillet(label):
     return Fillet_vertical
 
 # spoons
-Sketch_spoon13 = create_spoon_Sketch('spoon13', spoon13_handw, spoon13_handl, spoon13_bowlw, spoon13_bowll)
+Sketch_spoon13 = create_spoon_Sketch('spoon13', spoon13)
 Pad_spoon13 = create_Pad('spoon13')
 Fillet_spoon13 = create_Fillet('spoon13')
 
-Sketch_spoon15 = create_spoon_Sketch('spoon15', spoon15_handw, spoon15_handl, spoon15_bowlw, spoon15_bowll)
+Sketch_spoon15 = create_spoon_Sketch('spoon15', spoon15)
 Pad_spoon15 = create_Pad('spoon15')
 Fillet_spoon15 = create_Fillet('spoon15')
 
-Sketch_spoon18 = create_spoon_Sketch('spoon18', spoon18_handw, spoon18_handl, spoon18_bowlw, spoon18_bowll)
+Sketch_spoon18 = create_spoon_Sketch('spoon18', spoon18)
 Pad_spoon18 = create_Pad('spoon18')
 Fillet_spoon18 = create_Fillet('spoon18')
 
-Sketch_spoon21 = create_spoon_Sketch('spoon21', spoon21_handw, spoon21_handl, spoon21_bowlw, spoon21_bowll)
+Sketch_spoon21 = create_spoon_Sketch('spoon21', spoon21)
 Pad_spoon21 = create_Pad('spoon21')
 Fillet_spoon21 = create_Fillet('spoon21')
 
 # forks
-Sketch_fork14 = create_fork_Sketch('fork14',fork14_handw, fork14_handl, fork14_tinew, fork14_tinel)
+Sketch_fork14 = create_fork_Sketch('fork14',fork14)
 Pad_fork14 = create_Pad('fork14')
 Fillet_fork14 = create_Fillet('fork14')
 
-Sketch_fork15 = create_fork_Sketch('fork15',fork15_handw, fork15_handl, fork15_tinew, fork15_tinel)
+Sketch_fork15 = create_fork_Sketch('fork15',fork15)
 Pad_fork15 = create_Pad('fork15')
 Fillet_fork15 = create_Fillet('fork15')
 
-Sketch_fork18 = create_fork_Sketch('fork18',fork18_handw, fork18_handl, fork18_tinew, fork18_tinel)
+Sketch_fork18 = create_fork_Sketch('fork18',fork18)
 Pad_fork18 = create_Pad('fork18')
 Fillet_fork18 = create_Fillet('fork18')
 
-Sketch_fork15 = create_fork_Sketch('fork20',fork20_handw, fork20_handl, fork20_tinew, fork20_tinel)
+Sketch_fork15 = create_fork_Sketch('fork20',fork20)
 Pad_fork15 = create_Pad('fork20')
 Fillet_fork15 = create_Fillet('fork20')
 
+# knives
+Sketch_knive17 = create_knive_Sketch('knive17',knive17)
+Pad_knive17 = create_Pad('knive17')
+Fillet_knive17 = create_Fillet('knive17')
+
+Sketch_knive18 = create_knive_Sketch('knive18',knive18)
+Pad_knive18 = create_Pad('knive18')
+Fillet_knive18 = create_Fillet('knive18')
+
+Sketch_knive21 = create_knive_Sketch('knive21',knive21)
+Pad_knive21 = create_Pad('knive21')
+Fillet_knive21 = create_Fillet('knive21')
+
+Sketch_knive24 = create_knive_Sketch('knive24',knive24)
+Pad_knive24 = create_Pad('knive24')
+Fillet_knive24 = create_Fillet('knive24')
+
 # separating the objects
 rotation = App.Rotation(0, 0, 0)
+
 position = App.Vector(0, 0, 0)
 doc.getObject('Sketch_spoon13').Placement = FreeCAD.Placement(position, rotation) 
 position = App.Vector(100, 0, 0)
@@ -229,6 +247,7 @@ position = App.Vector(200, 0, 0)
 doc.getObject('Sketch_spoon18').Placement = FreeCAD.Placement(position, rotation) 
 position = App.Vector(300, 0, 0)
 doc.getObject('Sketch_spoon21').Placement = FreeCAD.Placement(position, rotation) 
+
 position = App.Vector(0, 300, 0)
 doc.getObject('Sketch_fork14').Placement = FreeCAD.Placement(position, rotation) 
 position = App.Vector(100, 300, 0)
@@ -237,6 +256,15 @@ position = App.Vector(200, 300, 0)
 doc.getObject('Sketch_fork18').Placement = FreeCAD.Placement(position, rotation) 
 position = App.Vector(300, 300, 0)
 doc.getObject('Sketch_fork20').Placement = FreeCAD.Placement(position, rotation) 
+
+position = App.Vector(0, 600, 0)
+doc.getObject('Sketch_knive17').Placement = FreeCAD.Placement(position, rotation) 
+position = App.Vector(100, 600, 0)
+doc.getObject('Sketch_knive18').Placement = FreeCAD.Placement(position, rotation) 
+position = App.Vector(200, 600, 0)
+doc.getObject('Sketch_knive21').Placement = FreeCAD.Placement(position, rotation) 
+position = App.Vector(300, 600, 0)
+doc.getObject('Sketch_knive24').Placement = FreeCAD.Placement(position, rotation) 
 
 doc.recompute()
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
